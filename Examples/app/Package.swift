c45c9666957d5d1198244fe500ee46c190f41a23
Dongjoon Hyun <dongjoon@apache.org>
2026-05-05 09:08:06 -0700

    [SPARK-56725] Upgrade Swift to 6.3.1

--- Examples/app/Package.swift
+++ Examples/app/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3
+// swift-tools-version: 6.3.1
 //
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
